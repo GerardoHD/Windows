11303753294283cc37517104325d497a2f1634a9
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 16:22:03 -0500

    Update Inicial.ps1

--- Scripts/Inicial.ps1
+++ Scripts/Inicial.ps1
@@ -1,4 +1,4 @@
 rm C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
-Set-Location $env:D:
-Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.ps1'
-.\Actualizar.ps1
+Set-Location $env:TEMP
+Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1'
+.\Descargar-Scripts.ps1
