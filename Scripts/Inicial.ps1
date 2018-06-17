5596cff1bc61b8ca9ffda65aa2394cfa9c2a7791
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 17:12:48 -0500

    Update Inicial.ps1

--- Scripts/Inicial.ps1
+++ Scripts/Inicial.ps1
@@ -1,3 +1,4 @@
 rm C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
-curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
-echo Ejecutar Descargar-Scripts
+Set-Location $env:TEMP
+Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1'
+.\Descargar-Scripts.ps1
