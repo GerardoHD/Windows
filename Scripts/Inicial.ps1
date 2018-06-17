cb52cc0bb953995f695316950a941cb96eb9567c
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 13:18:53 -0500

    Mejorada Instalacion de Script

--- Scripts/Inicial.ps1
+++ Scripts/Inicial.ps1
@@ -1,2 +1,3 @@
+rm C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
 curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
 echo Ejecutar Descargar-Scripts
