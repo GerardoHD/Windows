4db2df0748ee7721f4d5d3892c67f4a1956394f1
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 13:12:18 -0500

    Add files via upload

---
+++ Scripts/Inicial.ps1
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
+echo Ejecutar Descargar-Scripts
