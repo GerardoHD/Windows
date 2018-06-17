5e8efdae0a55c46ec7bad9ea6c7a9dc40b92945d
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 12:52:07 -0500

    Update Instalador.ps1

--- Scripts/Instalador.ps1
+++ Scripts/Instalador.ps1
@@ -4,6 +4,7 @@ curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualiz
 curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Base.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Base.ps1
 curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/GHD.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\GHD.ps1
 curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Software.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Software.ps1
+rm C:\Program` Files\WindowsPowerShell\Scripts\Instalador.ps1
 curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Instalador.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Instalador
 .ps1
 echo Ejecutar Uno
