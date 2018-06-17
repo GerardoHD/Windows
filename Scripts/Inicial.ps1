65f2355c7b8feab222dfd175f02749754c2ed81c
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 16:44:41 -0500

    Revertido

--- Scripts/Inicial.ps1
+++ Scripts/Inicial.ps1
@@ -1,5 +1,3 @@
-Set-Location $env:TEMP
-Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Sripts.ps1'
-.\Descargar-Scripts.ps1
-echo Ejecutar Uno de los siguientes Scripts
-ls C:\Program` Files\WindowsPowerShell\Scripts\
+rm C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
+curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
+echo Ejecutar Descargar-Scripts
