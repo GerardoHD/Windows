e58ff335cc22889a0ae53656cd6337099bb6d92f
Gerardo Herrera Diaz <Gerardoherreradiaz@gmail.com>
2018-06-17 16:20:19 -0500

    Reestructuración del Script

--- Scripts/Inicial.ps1
+++ Scripts/Inicial.ps1
@@ -1,3 +1,4 @@
 rm C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
-curl https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1 -o C:\Program` Files\WindowsPowerShell\Scripts\Descargar-Scripts.ps1
-echo Ejecutar Descargar-Scripts
+Set-Location $env:D:
+Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.ps1'
+.\Actualizar.ps1
