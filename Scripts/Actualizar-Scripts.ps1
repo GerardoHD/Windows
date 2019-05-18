83af56a6f0918088c69796e36cf4dd2297687624
GerardoHD <gerardoherreradiaz@gmail.com>
2019-05-17 20:53:47 -0500

    Cambiada dirección de descarga de Scripts a C:/Scripts

--- Scripts/Actualizar-Scripts.ps1
+++ Scripts/Actualizar-Scripts.ps1
@@ -1,4 +1,4 @@
-rm D:/Documentos/WindowsPowershell/Scripts/Descargar-Scripts.ps1
+rm C:/Scripts/Descargar-Scripts.ps1
 Set-Location $env:TEMP
 Start-BitsTransfer -Source 'https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Descargar-Scripts.ps1'
 .\Descargar-Scripts.ps1
--- Scripts/Descargar-Scripts.ps1
+++ Scripts/Descargar-Scripts.ps1
@@ -1,10 +1,10 @@
-rm D:/Documentos/WindowsPowershell/Scripts/Actualizar.bat
-rm D:/Documentos/WindowsPowershell/Scripts/Actualizar.ps1
-rm D:/Documentos/WindowsPowershell/Scripts/Actualizar-Scripts.ps1
-rm D:/Documentos/WindowsPowershell/Scripts/Windows-Setup.ps1
-Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.bat -Destination D:/Documentos/WindowsPowershell/Scripts/Actualizar.bat
-Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.ps1 -Destination D:/Documentos/WindowsPowershell/Scripts/Actualizar.ps1
-Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar-Scripts.ps1 -Destination D:/Documentos/WindowsPowershell/Scripts/Actualizar-Scripts.ps1
-Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Base.ps1 -Destination D:/Documentos/WindowsPowershell/Scripts/Windows-Setup.ps1
+rm C:/Scripts/Actualizar.bat
+rm C:/Scripts/Actualizar.ps1
+rm C:/Scripts/Actualizar-Scripts.ps1
+rm C:/Scripts/Windows-Setup.ps1
+Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.bat -Destination C:/Scripts/Actualizar.bat
+Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar.ps1 -Destination C:/Scripts/Actualizar.ps1
+Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Actualizar-Scripts.ps1 -Destination C:/Scripts/Actualizar-Scripts.ps1
+Start-BitsTransfer https://raw.githubusercontent.com/GerardoHD/Windows/master/Scripts/Base.ps1 -Destination C:/Scripts/Windows-Setup.ps1
 echo Ejecutar Uno de los siguientes Scripts
-ls D:/Documentos/WindowsPowershell/Scripts/
+ls C:/Scripts/
